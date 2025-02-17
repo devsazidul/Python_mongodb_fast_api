a2921001fc8abadc86fe3db840fd6b244c9e0968
bdCalling <bdcalling@bdCallings-Mac-mini.local>
2025-02-17 17:51:09 +0600

    Api Reddy korci amk jast mongodb ta connect korta hobe akn

---
+++ main.py
@@ -0,0 +1,49 @@
+from fastapi import FastAPI
+from typing import List
+from pydantic import BaseModel
+from motor.motor_asyncio import AsyncIOMotorClient
+
+# rabbiking00
+# rQauiItdlNdL7hdX
+
+app = FastAPI()
+
+MONGO_URL = "mongodb+srv://rabbiking00:<sazidul133>@cluster0.z6b9l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+DB_NAME = "mydatabase"
+COLLECTION_NAME = "user_data"
+
+
+# MongoDB client initialization on startup
+@app.on_event("startup")
+async def startup_db():
+    app.client = AsyncIOMotorClient(MONGO_URL) 
+    app.db = app.client[DB_NAME]
+    app.collection = app.db[COLLECTION_NAME]
+
+
+# Close the MongoDB connection on shutdown
+@app.on_event("shutdown")
+async def shutdown_db():
+    app.client.close()
+
+
+class Data(BaseModel):
+    fullname: str
+    schoolname: str
+    role: str
+    age: int
+
+@app.post("/postdata")
+async def postdata(data: Data):
+    user_data = data.dict() 
+    result = await app.collection.insert_one(user_data) 
+    return {"message": "data added successfully", "id": str(result.inserted_id)}
+
+
+@app.get("/getdata", response_model=List[Data])
+async def getdata():
+    users = []
+    async for user in app.collection.find():
+        user['_id'] = str(user['_id'])
+        users.append(user)
+    return users
